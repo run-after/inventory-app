281e8862f0166a0e5c15167a6236c9e4c6bb47ed
run-after <run.after@protonmail.com>
2021-05-18 12:51:20 -0500

    Add manufacturer

--- models/item.js
+++ models/item.js
@@ -4,6 +4,7 @@ const Schema = mongoose.Schema;
 
 const ItemSchema = new Schema(
   {
+    manufacturer: {type: String, required: true, maxLength: 100},
     name: { type: String, required: true, maxLength: 100 },
     description: { type: String, required: true, maxLength: 100 },
     category: { type: Schema.Types.ObjectId, ref: 'Category', required: true },
