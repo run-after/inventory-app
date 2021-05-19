bc072bbf69670ebccdfef7bd27333fbabc1606df
run-after <run.after@protonmail.com>
2021-05-19 09:18:17 -0500

    Extend allowed length of description

--- models/item.js
+++ models/item.js
@@ -6,7 +6,7 @@ const ItemSchema = new Schema(
   {
     manufacturer: {type: String, required: true, maxLength: 100},
     name: { type: String, required: true, maxLength: 100 },
-    description: { type: String, required: true, maxLength: 100 },
+    description: { type: String, required: true, maxLength: 1000 },
     category: { type: Schema.Types.ObjectId, ref: 'Category', required: true },
     price: { type: Number, required: true },
     quantity: { type: Number, required: true }
