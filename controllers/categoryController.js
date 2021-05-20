c8d30b777acaa903d71d438d4c84b8b8f1a66c72
run-after <run.after@protonmail.com>
2021-05-20 13:21:03 -0500

    Finish category detail and list methods

--- controllers/categoryController.js
+++ controllers/categoryController.js
@@ -1,10 +1,11 @@
 const Category = require('../models/category');
+const Item = require('../models/item');
 
 const async = require('async');
 const { body, validationResult } = require('express-validator');
 
 exports.index = function (req, res) {
-  res.render('index', {title: 'Inventory'})
+  res.render('index', { title: 'Inventory' });
 };
 
 exports.category_create_get = function (req, res) {
@@ -31,10 +32,29 @@ exports.category_update_post = function (req, res) {
   res.send('category/update (post)')
 };
 
-exports.category_detail = function (req, res) {
-  res.send('category/:id (get)')
+exports.category_detail = function (req, res, next) {
+  async.parallel({
+    category: function (callback) {
+      Category.findById(req.params.id).exec(callback);
+    },
+    category_items: function (callback) {
+      Item.find({ 'category': req.params.id }).exec(callback);
+    },
+  },function (err, results) {
+    if (err) { return next(err); }
+    if (results.category == null) {
+      const err = new Error('Category not found');
+      err.status = 404;
+      return next(err);
+    }
+    res.render('category_detail', { title: results.category.name, category: results.category, items: results.category_items });
+  });
 };
 
 exports.category_list = function (req, res) {
-  res.send('categories (get)')
+  Category.find().sort([['name', 'ascending']])
+    .exec(function (err, list_categories) {
+      if (err) { return async.nextTick(err) };
+      res.render('category_list', { title: 'Category List', category_list: list_categories })
+    });
 };
