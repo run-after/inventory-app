359d7a3bed266debae5a82dd510d2151d02ea6cb
run-after <run.after@protonmail.com>
2021-05-27 13:16:33 -0500

    Utilize multer to save image to disk

--- routes/index.js
+++ routes/index.js
@@ -1,6 +1,21 @@
 const express = require('express');
 const router = express.Router();
 
+const multer = require('multer');
+
+var storage = multer.diskStorage({
+  destination: function (req, file, cb) {
+    cb(null, './public/images/')
+  },
+  filename: function (req, file, cb) {
+    cb(null, file.originalname)
+  }
+});
+
+var upload = multer({
+  storage: storage
+})
+
 const item_controller = require('../controllers/itemController');
 const category_controller = require('../controllers/categoryController');
 
@@ -37,7 +52,7 @@ router.get('/categories', category_controller.category_list);
 router.get('/item/create', item_controller.item_create_get);
 
 // POST request for creating Item.
-router.post('/item/create', item_controller.item_create_post);
+router.post('/item/create', upload.single('uploaded_file'), item_controller.item_create_post);
 
 // GET request to delete Item.
 router.get('/item/:id/delete', item_controller.item_delete_get);
@@ -56,11 +71,5 @@ router.get('/item/:id', item_controller.item_detail);
 
 // GET reqest for list of all items
 router.get('/items', item_controller.item_list);
-
-
-
-
-
-
 
 module.exports = router;
