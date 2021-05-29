7e533637ec4702445619de728f6f0b80478fc4ac
run-after <run.after@protonmail.com>
2021-05-29 12:34:49 -0500

    Get ready for deployment

--- app.js
+++ app.js
@@ -11,7 +11,7 @@ var app = express();
 
 // Set up mongoose connection
 var mongoose = require('mongoose');
-var mongoDB = 'mongodb+srv://runafter:Chase!23@cluster0.6aj6a.mongodb.net/dr-builder?retryWrites=true&w=majority'
+var mongoDB = process.env.MONGODB_URI;
 mongoose.connect(mongoDB, { useNewUrlParser: true , useUnifiedTopology: true});
 var db = mongoose.connection;
 db.on('error', console.error.bind(console, 'MongoDB connection error:'));
